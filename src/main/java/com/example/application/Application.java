664d7b584250a147b6e7b47d6d8dc24e18dcc77f
Beni <szekrenyes.beni@gmail.com>
2023-06-10 17:48:00 +0300

    testing production

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -5,11 +5,17 @@ import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @Theme(value = "auctionhouse", variant = Lumo.DARK)
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator  {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
